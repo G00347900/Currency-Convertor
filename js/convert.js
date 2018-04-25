a12f27450792b7eff04ece1a4bc36a607caa997d
G00347900 <35769360+G00347900@users.noreply.github.com>
2018-04-25 13:44:59 +0100

    Converting working for each currency
    Also have placed 4 comments in

--- js/convert.js
+++ js/convert.js
@@ -1,9 +1,46 @@
+//Converting Euro to Sterling
 function euroToPounds(){
 
   var amount = parseInt(document.getElementById("value1").value);
-
+  
   var pounds = amount * 0.86;
   var message = amount + ' euros converts to ' + pounds + ' sterling.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+
+//Converting Euro to Us Dollars 
+function euroToDollars(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+  
+  var dollars = amount * 1.06;
+  var message = amount + ' euros converts to ' + dollars + ' dollars.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+  
+}
+
+//Converting Euro to Japanese Yen
+function euroToYen(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+  
+  var Yen = amount * 121.12;
+  var message = amount + ' euros converts to ' + Yen + ' Yen.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+  
+}
+
+//Converting Euro to Bitcoin
+function euroToBitcoin(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+  
+  var Bitcoin = amount * 0.00082;
+  var message = amount + ' euros converts to ' + Bitcoin + ' Bitcoin.';
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
 
